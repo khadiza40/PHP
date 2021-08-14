abc20ae17a22280d34b183b4d19b0cc34e5d9445
BK -40 <bkhadiza277@gmail.com>
2021-08-14 16:02:28 +0600

    Date and Time with date functionn example code in php

---
+++ Day02/DateAndTime.php
@@ -0,0 +1,17 @@
+<!-- DateAndTime.php -->
+<?php 
+//date find
+ date_default_timezone_set("Asia/Dhaka") . "<br";
+echo "Today time is " . date("h:i:s") . "<br";
+echo "Today time is " . date("h:i:sa") . "<br";
+echo "Today date is " . date("d.m.y") . "<br";
+echo "Today date is " . date("Y") . "<br";
+echo "Today date is " . date("D.M.Y") . "<br";
+echo "Today date is " . date("Y-M-D") . "<br";
+echo "Today date is " . date("Y/M/D") . "<br";
+echo "Today hour is " . date("h") . "<br";
+echo "Today time is " . date("h:i") . "<br";
+echo "Today date is " . date("D") . "<br";
+
+
+?>
